20670cb2e74cfa7807e7c72c3029adaa7718b548
parthsharma-7 <149650634+parthsharma-7@users.noreply.github.com>
2025-08-28 10:54:46 +0530

    Add files via upload

---
+++ string/Problem1_Level2.java
@@ -0,0 +1,30 @@
+package Strings;
+
+import java.util.Scanner;
+
+class Problem1_Level2 {
+    public static int customLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        int custom = customLength(text);
+        int builtin = text.length();
+
+        System.out.println("Custom length: " + custom);
+        System.out.println("Built-in length: " + builtin);
+    }
+}
+
